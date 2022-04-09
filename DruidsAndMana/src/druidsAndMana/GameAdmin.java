a04eb29763c94574464a26af609f793dc68cf6d1
15197069 <thompsond26@qub.ac.uk>
2022-04-09 15:34:12 +0100

    Added GameAdmin class and GameAdminTests class. Testing commit and merge function

---
+++ DruidsAndMana/src/druidsAndMana/GameAdmin.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package druidsAndMana;
+
+/**
+ * @author Dave
+ *
+ */
+public class GameAdmin {
+	
+	private IInputService inputService;
+	
+	public GameAdmin(IInputService inputService) {
+		this.inputService = inputService;
+	}
+	
+	public int numOfPlayers() {
+	//Ask number of players
+	String numOfPlayers = this.inputService.GetUserInput("How many players are joining the game?", new String[] {"1","2","3","4"});
+	
+	//confirm selection
+	boolean check = this.inputService.GetUserConfirmation("Are you sure?");
+	
+	//if confirmed, return number of players
+	if(check) {
+		return Integer.parseInt(numOfPlayers);
+	}else {
+		return 0;
+	}
+	}
+	
+	
+
+}
