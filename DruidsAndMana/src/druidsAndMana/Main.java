8d5b3bb35bf25aeb267996ea07019ffd1113db18
15197069 <thompsond26@qub.ac.uk>
2022-04-12 16:36:34 +0100

    Fixed playerPosition Getter

--- DruidsAndMana/src/druidsAndMana/Main.java
+++ DruidsAndMana/src/druidsAndMana/Main.java
@@ -10,15 +10,17 @@ public class Main {
 		
 		admin.playerSetUp(admin.numOfPlayers());
 		admin.createGameBoard();
-		System.out.println(admin.isSquareOwned(admin.board, 2) + "\n");
-		System.out.println(admin.board.squareIsGrassland(2) + "\n");
-		System.out.println(admin.board.costToUpgrade(2) + "\n");
+		System.out.println(admin.isSquareOwned(admin.board, 4) + "\n");
+		System.out.println(admin.board.squareIsGrassland(4) + "\n");
+		System.out.println(admin.board.costToUpgrade(4) + "\n");
 		for (Player player : admin.players) {
 			System.out.println(player.toString());
 		}
 		admin.movePlayer(4);
 		admin.displaySquareDetails();
-		admin.buyUnownedGrassland(admin.board, admin.getCurrentPlayerPosition(), admin.players.get(admin.currentPlayer).getPlayerNumber());
+		admin.buyUnownedGrassland(admin.board, 4, 1);
 		System.out.println("\n\n\n" +admin.isSquareOwned(admin.board, 4));
+		int owner = admin.board.getSquareOwnerId(4);
+		System.out.println(admin.players.get(owner-1).toString());
 	}
 }
