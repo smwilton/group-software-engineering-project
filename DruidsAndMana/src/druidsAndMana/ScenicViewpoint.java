189c1ce39a1504969cf318071a9a7565f85aa355
15197069 <thompsond26@qub.ac.uk>
2022-04-19 18:06:51 +0100

    Changes to console output, more in keeping with environmental theme. Changes to upgrade-menu system outputs

--- DruidsAndMana/src/druidsAndMana/ScenicViewpoint.java
+++ DruidsAndMana/src/druidsAndMana/ScenicViewpoint.java
@@ -12,7 +12,8 @@ public class ScenicViewpoint implements ISquare {
 
 	@Override
 	public String description() {
-		return "The scenic viewpoint represents a safe place for the druids to stop. There are no consequences for landing on this square";
+		return "The scenic viewpoint represents a safe place for the druids to stop. There are no consequences for landing on this square.\n"
+				+ "So take a seat and gaze upon the landscape as the druids replenish it's splendour!";
 	}
 
 	@Override
